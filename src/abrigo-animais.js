fbcfe2546c13573d0d6c471c2cb9fd1e6152e00f
Leonan Louvem <leonanlouvem2015@gmail.com>
2025-09-09 20:37:53 -0300

    Versão Final

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,176 +1,198 @@
 class AbrigoAnimais {
-  
-  constructor(){
-
+  constructor() {
     this.tabelaAnimais = {
-      'Rex': { tipo: 'cão', brinquedos: ['RATO', 'BOLA'] },
-      'Mimi': { tipo: 'gato', brinquedos: ['BOLA', 'LASER'] },
-      'Fofo': { tipo: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] },
-      'Zero': { tipo: 'gato', brinquedos: ['RATO', 'BOLA'] },
-      'Bola': { tipo: 'cão', brinquedos: ['CAIXA', 'NOVELO'] },
-      'Bebe': { tipo: 'cão', brinquedos: ['LASER', 'RATO', 'BOLA'] },
-      'Loco': { tipo: 'jabuti', brinquedos: ['SKATE', 'RATO'] }
-    }
-    this.brinquedosPossiveis = new Set()
-    for (const animalInfo of Object.values(this.tabelaAnimais)){
-      for(const brinquedo of animalInfo.brinquedos){
+      Rex: { tipo: "cão", brinquedos: ["RATO", "BOLA"] },
+      Mimi: { tipo: "gato", brinquedos: ["BOLA", "LASER"] },
+      Fofo: { tipo: "gato", brinquedos: ["BOLA", "RATO", "LASER"] },
+      Zero: { tipo: "gato", brinquedos: ["RATO", "BOLA"] },
+      Bola: { tipo: "cão", brinquedos: ["CAIXA", "NOVELO"] },
+      Bebe: { tipo: "cão", brinquedos: ["LASER", "RATO", "BOLA"] },
+      Loco: { tipo: "jabuti", brinquedos: ["SKATE", "RATO"] },
+    };
+    this.brinquedosPossiveis = new Set();
+    for (const animalInfo of Object.values(this.tabelaAnimais)) {
+      for (const brinquedo of animalInfo.brinquedos) {
         this.brinquedosPossiveis.add(brinquedo);
       }
     }
-
   }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-
     //PRIMEIRO TRATO AS STRINGS
-    const brinquedosUm = brinquedosPessoa1.split(',');
-    const brinquedosDois = brinquedosPessoa2.split(',');
-    const entradaAnimais = ordemAnimais.split(',');
+    const brinquedosUm = brinquedosPessoa1.split(",");
+    const brinquedosDois = brinquedosPessoa2.split(",");
+    const entradaAnimais = ordemAnimais.split(",");
 
     //VALIDO AS ENTRADAS
-    const erro = this.#validaEntradas(entradaAnimais, brinquedosUm, brinquedosDois);
+    const erro = this.#validaEntradas(
+      entradaAnimais,
+      brinquedosUm,
+      brinquedosDois
+    );
     if (erro) {
       return erro;
     }
 
     let adocoes = [];
-    let qtdPessoaUm = 0;
-    let qtdPessoaDois = 0;
     let adotadosPelaPessoaUm = [];
     let adotadosPelaPessoaDois = [];
 
-
-    for (const animal of entradaAnimais){
-      
-      if (animal === 'Loco') {
+    //TESTO E DECIDO CADA ANIMAL (MENOS O LOCO)
+    for (const animal of entradaAnimais) {
+      if (animal === "Loco") {
         continue;
       }
       const animalInfo = this.tabelaAnimais[animal];
-      
+
       //VERIFICO SE ALGUEM PODE ADOTAR O ANIMAL E ADICIONO NA LISTA DE ANIMAIS
-      const pessoaUmPode = this.#podeAdotar(brinquedosUm, animalInfo.brinquedos);
-      const pessoaDoisPode = this.#podeAdotar(brinquedosDois, animalInfo.brinquedos);
+      const pessoaUmPode = this.#podeAdotar(
+        brinquedosUm,
+        animalInfo.brinquedos
+      );
+      const pessoaDoisPode = this.#podeAdotar(
+        brinquedosDois,
+        animalInfo.brinquedos
+      );
       let destinoFinal = `${animal} - abrigo`;
-      
-      if(pessoaUmPode && !pessoaDoisPode && qtdPessoaUm < 3){
-        const temConflito = this.#conflitoGato(animalInfo, adotadosPelaPessoaUm);
-        if(!temConflito){
+
+      if (pessoaUmPode && !pessoaDoisPode && adotadosPelaPessoaUm.length < 3) {
+        const temConflito = this.#conflitoGato(
+          animalInfo,
+          adotadosPelaPessoaUm
+        );
+        if (!temConflito) {
           destinoFinal = `${animal} - pessoa 1`;
-          qtdPessoaUm++;
           adotadosPelaPessoaUm.push(animal);
         }
-      }
-      else if(!pessoaUmPode && pessoaDoisPode && qtdPessoaDois < 3){
-        const temConflito = this.#conflitoGato(animalInfo, adotadosPelaPessoaDois);
-        if(!temConflito){
+      } else if (
+        !pessoaUmPode &&
+        pessoaDoisPode &&
+        adotadosPelaPessoaDois.length < 3
+      ) {
+        const temConflito = this.#conflitoGato(
+          animalInfo,
+          adotadosPelaPessoaDois
+        );
+        if (!temConflito) {
           destinoFinal = `${animal} - pessoa 2`;
-          qtdPessoaDois++;
           adotadosPelaPessoaDois.push(animal);
         }
       }
       adocoes.push(destinoFinal);
-
     }
 
-    if (entradaAnimais.includes('Loco')){
-      
+    // PARA O LOCO
+    if (entradaAnimais.includes("Loco")) {
       let pessoaUmPodeLoco = false;
-      if (qtdPessoaUm > 0) { 
-        pessoaUmPodeLoco = this.#podeAdotarLoco(brinquedosUm); 
+      if (adotadosPelaPessoaUm.length > 0) {
+        pessoaUmPodeLoco = this.#podeAdotarLoco(brinquedosUm);
       } else {
-        pessoaUmPodeLoco = this.#podeAdotar(brinquedosUm, this.tabelaAnimais['Loco'].brinquedos);
+        pessoaUmPodeLoco = this.#podeAdotar(
+          brinquedosUm,
+          this.tabelaAnimais["Loco"].brinquedos
+        );
       }
 
       let pessoaDoisPodeLoco = false;
-      if (qtdPessoaDois > 0) { 
-        pessoaDoisPodeLoco = this.#podeAdotarLoco(brinquedosDois); 
+      if (adotadosPelaPessoaDois.length > 0) {
+        pessoaDoisPodeLoco = this.#podeAdotarLoco(brinquedosDois);
       } else {
-        pessoaDoisPodeLoco = this.#podeAdotar(brinquedosDois, this.tabelaAnimais['Loco'].brinquedos);
+        pessoaDoisPodeLoco = this.#podeAdotar(
+          brinquedosDois,
+          this.tabelaAnimais["Loco"].brinquedos
+        );
       }
 
-      let destinoLoco = 'Loco - abrigo';
-      if (pessoaUmPodeLoco && !pessoaDoisPodeLoco && qtdPessoaUm < 3) {
-        destinoLoco = 'Loco - pessoa 1';
-      } else if (pessoaDoisPodeLoco && !pessoaUmPodeLoco && qtdPessoaDois < 3) {
-        destinoLoco = 'Loco - pessoa 2';
+      let destinoLoco = "Loco - abrigo";
+      if (
+        pessoaUmPodeLoco &&
+        !pessoaDoisPodeLoco &&
+        adotadosPelaPessoaUm.length < 3
+      ) {
+        destinoLoco = "Loco - pessoa 1";
+      } else if (
+        pessoaDoisPodeLoco &&
+        !pessoaUmPodeLoco &&
+        adotadosPelaPessoaDois.length < 3
+      ) {
+        destinoLoco = "Loco - pessoa 2";
       }
-      
+
       adocoes.push(destinoLoco);
-
     }
 
     const saida = adocoes.sort();
     return { lista: saida };
-    
   }
 
-
-  #podeAdotarLoco(brinquedosPessoa){
-    const brinquedosLoco = this.tabelaAnimais['Loco'].brinquedos;
-    return brinquedosLoco.every(brinquedo => brinquedosPessoa.includes(brinquedo));
+  #podeAdotarLoco(brinquedosPessoa) {
+    const brinquedosLoco = this.tabelaAnimais["Loco"].brinquedos;
+    return brinquedosLoco.every((brinquedo) =>
+      brinquedosPessoa.includes(brinquedo)
+    );
   }
 
-  #conflitoGato(animalInfo, adotadosPessoa){
-    if (animalInfo.tipo !== 'gato'){
+  // TESTA SE O ANIMAL DA VEZ (GATO) POSSUI BRINQUEDO EM COMUM COM ALGUM JA ADOTADO
+  #conflitoGato(animalInfo, adotadosPessoa) {
+    if (animalInfo.tipo !== "gato") {
       return false;
     }
-    for (const outroAnimalNome of adotadosPessoa){
-      const outroAnimalInfo = this.tabelaAnimais[outroAnimalNome]
-      const temBrinquedoEmComum = animalInfo.brinquedos.some(brinquedo =>outroAnimalInfo.brinquedos.includes(brinquedo))
-      
-      if(temBrinquedoEmComum){
+    for (const outroAnimalNome of adotadosPessoa) {
+      const outroAnimalInfo = this.tabelaAnimais[outroAnimalNome];
+      const temBrinquedoEmComum = animalInfo.brinquedos.some((brinquedo) =>
+        outroAnimalInfo.brinquedos.includes(brinquedo)
+      );
+
+      if (temBrinquedoEmComum) {
         return true;
       }
-      
-    } 
+    }
     return false;
   }
 
-  #podeAdotar(brinquedosPessoa, brinquedosAnimal){
+  #podeAdotar(brinquedosPessoa, brinquedosAnimal) {
     let i = 0;
-    for (const brinquedo of brinquedosPessoa){
-      if (brinquedo === brinquedosAnimal[i]){
+    for (const brinquedo of brinquedosPessoa) {
+      if (brinquedo === brinquedosAnimal[i]) {
         i++;
       }
-      if (i === brinquedosAnimal.length){
-        break
+      if (i === brinquedosAnimal.length) {
+        break;
       }
     }
-    return i === brinquedosAnimal.length
+    return i === brinquedosAnimal.length;
   }
 
-  #validaEntradas(entradaAnimais, brinquedosUm, brinquedosDois){
-
+  #validaEntradas(entradaAnimais, brinquedosUm, brinquedosDois) {
     //VALIDO OS NOMES DOS ANIMAIS
-    
-    if (new Set(entradaAnimais).size !== entradaAnimais.length){
-      return { erro: 'Animal inválido' };
+
+    if (new Set(entradaAnimais).size !== entradaAnimais.length) {
+      return { erro: "Animal inválido" };
     }
-    for (const animal of entradaAnimais){
-      if (this.tabelaAnimais[animal] === undefined){
-        return { erro: 'Animal inválido' };
-  
+    for (const animal of entradaAnimais) {
+      if (this.tabelaAnimais[animal] === undefined) {
+        return { erro: "Animal inválido" };
       }
     }
 
-    // VALIDO OS BRINQUEDOS 
+    // VALIDO OS BRINQUEDOS
 
-    if (new Set(brinquedosUm).size !== brinquedosUm.length){
-      return { erro: 'Brinquedo inválido' };
+    if (new Set(brinquedosUm).size !== brinquedosUm.length) {
+      return { erro: "Brinquedo inválido" };
     }
-    if (new Set(brinquedosDois).size !== brinquedosDois.length){
-      return { erro: 'Brinquedo inválido' };
+    if (new Set(brinquedosDois).size !== brinquedosDois.length) {
+      return { erro: "Brinquedo inválido" };
     }
-    
+
     for (const brinquedo of brinquedosUm) {
-      if (!this.brinquedosPossiveis.has(brinquedo)) { 
-        return { erro: 'Brinquedo inválido' };
+      if (!this.brinquedosPossiveis.has(brinquedo)) {
+        return { erro: "Brinquedo inválido" };
       }
     }
-  
+
     for (const brinquedo of brinquedosDois) {
-      if (!this.brinquedosPossiveis.has(brinquedo)) { 
-        return { erro: 'Brinquedo inválido' };
+      if (!this.brinquedosPossiveis.has(brinquedo)) {
+        return { erro: "Brinquedo inválido" };
       }
     }
 
